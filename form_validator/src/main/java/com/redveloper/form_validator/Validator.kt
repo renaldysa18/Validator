b44f76ad9d11f29dc948d6f10032bc938eee5f92
Admin <admin@Admins-MBP.lan>
2019-10-02 00:59:49 +0700

    inital validasi login with email and username

--- form_validator/src/main/java/com/redveloper/form_validator/Validator.kt
+++ form_validator/src/main/java/com/redveloper/form_validator/Validator.kt
@@ -1,14 +1,16 @@
 package com.redveloper.form_validator
 
+import android.util.Patterns
+
 class Validator {
     fun validasiLoginWithUsername(username: String, password: String, callback: (result: Boolean, message: String) -> Unit){
         if(!username.isEmpty()){
             if(username.length >= 3){
                 if(!password.isEmpty()){
-                    if(password.length >= 6){
+                    if(password.length >= 8){
                         callback.invoke(true, "login successful")
                     } else {
-                        callback.invoke(false, "password is less than 6 character")
+                        callback.invoke(false, "password must be at least 8 characters")
                     }
                 } else {
                     callback.invoke(false, "password is empety")
@@ -18,6 +20,26 @@ class Validator {
             }
         } else {
             callback.invoke(false, "username is empety")
+        }
+    }
+
+    fun validasiLoginWithEmail(email: String, password: String, callback: (result: Boolean, message: String) -> Unit){
+        if(!email.isEmpty()){
+            if(Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                if(!password.isEmpty()){
+                    if(password.length >= 8){
+                        callback.invoke(true, "login successful")
+                    } else {
+                        callback.invoke(false, "password must be at least 8 characters")
+                    }
+                } else {
+                    callback.invoke(false, "password is empety")
+                }
+            } else{
+                callback.invoke(false, "email is not valid")
+            }
+        } else {
+            callback.invoke(false, "email is empety")
         }
     }
 }
